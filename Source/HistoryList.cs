f2d502f3b009402a83a842eb4f739bc4e7da423b
Fluffy <fluffy.l2032@gmail.com>
2020-12-08 19:09:35 +0100

    Release 1.8.113 [nolog]

--- Source/HistoryList.cs
+++ Source/HistoryList.cs
@@ -6,27 +6,14 @@ using System.Collections.Generic;
 
 namespace CommandPalette
 {
-    public class HistoryList<T>: IEnumerable<T>
+    public class HistoryList<T> : IEnumerable<T>
     {
-        private List<T> data = new List<T>();
-        private int size;
+        private readonly List<T> data = new List<T>();
+        private          int     size;
 
-        public HistoryList( int size )
+        public HistoryList(int size)
         {
             this.size = size;
-        }
-
-        public void Resize( int size ){
-            this.size = size;
-        }
-
-        public void Add( T datum )
-        {
-            if ( data.Contains( datum ) )
-                data.Remove( datum );
-            data.Insert( 0, datum );
-            while ( data.Count > size )
-                data.RemoveAt( size );
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -37,6 +24,25 @@ namespace CommandPalette
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public void Resize(int size)
+        {
+            this.size = size;
+        }
+
+        public void Add(T datum)
+        {
+            if (data.Contains(datum))
+            {
+                data.Remove(datum);
+            }
+
+            data.Insert(0, datum);
+            while (data.Count > size)
+            {
+                data.RemoveAt(size);
+            }
         }
     }
 }
